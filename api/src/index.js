bb8fcb19a04e3df3358271e8d68d7412feb9b555
Pedro Henrique <pholiveirabrito@hotmail.com>
2024-06-10 22:58:05 -0300

    fix: endpoint de login

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,8 @@
 // Para conseguir acessar as variáveis de ambiente no arquivo .env
 import 'dotenv/config'
-// Variável de conexão com o banco de dados
-import { con } from './repository/connection.js'
+
+import usuarioController from './controller/usuarioController.js'
+
 // 
 import express from 'express'
 // Uma especificação que faz uso de headers do HTTP para informar aos navegadores se determinado recurso pode ser ou não acessado.
@@ -9,8 +10,10 @@ import cors from 'cors'
 
 const server = express();
 server.use(cors());
-
 // Ativa a opção para que os parâmetros de corpo da requisição possam ser enviados no formato json
 server.use(express.json());
+
+// Config dos endpoints
+server.use(usuarioController);
 
 server.listen(process.env.PORT, () => console.log(`API Conectada na Porta ${process.env.PORT}`))
